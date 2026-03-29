9a7e34e34ebf5e9b91a6be7e4ce90afbb613d534
Akshatt02 <akshatjaipuriar@gmail.com>
2026-03-29 23:26:58 +0530

    new files

--- practice.py
+++ practice.py
@@ -1,23 +1,46 @@
-import cv2
-import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
-from skimage.util import random_noise
+from tensorflow.keras import layers, models
 
-def show(img, title=""):
-  plt.figure(figsize=(4,4))
-  plt.imshow(img, cmap='gray')
-  plt.title(title)
-  plt.axis('off')
-  plt.show()
+ds_train, ds_test = tfds.load(
+    "cats_vs_dogs",
+    split=["train[:80%]", "train[80%:90%]"],
+    as_supervised=True,
+)
 
-"""ASSIGNMENT 1"""
+def preprocess(img, label):
+    img = tf.image.resize(img, (64,64)) / 255.0
+    return img,label
 
-img = cv2.imread('sample.jpg', 0) / 255.0
-noisy = random_noise(img, mode='gaussian', mean=0, var=0.05)
-noisy = (noisy * 255).astype(np.uint8)
-filtered = cv2.blur(noisy, (5,5))
+train_dataset = ds_train.map(preprocess).batch(32)
+test_dataset = ds_test.map(preprocess).batch(32)
 
-plt.figure(figsize=(4,4))
-plt.subplot(1,2,1); plt.imshow(noisy, cmap='gray'); plt.title("Noisy Image"); plt.axis('off')
-plt.subplot(1,2,2); plt.imshow(filtered, cmap='gray'); plt.title("Filtered Image"); plt.axis('off')
+model = models.Sequential([
+    layers.Conv2D(32, 3, activation='relu', input_shape=(64,64,3)),
+    layers.MaxPool2D(),
+
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPool2D(),
+
+    layers.Conv2D(128, 3, activation='relu'),
+    layers.MaxPool2D(),
+
+    layers.Flatten(),
+    layers.Dense(256, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(train_dataset, validation_data=test_dataset, epochs=1)
+
+plt.plot(history.history["accuracy"], label="train")
+plt.plot(history.history["val_accuracy"], label="val")
+plt.legend()
 plt.show()
